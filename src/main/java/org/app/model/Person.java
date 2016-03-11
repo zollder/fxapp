4404fa363512c52f9236ffccbdc3c57d14f479e9
Eugene Simon <goodzollder@gmail.com>
2016-03-11 02:45:55 -0500

    First commit to JavaFX playground app.

---
+++ src/main/java/org/app/model/Person.java
@@ -0,0 +1,127 @@
+package org.app.model;
+
+import java.time.LocalDate;
+
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+public class Person
+{
+	private StringProperty firstName;
+	private StringProperty lastName;
+	private StringProperty street;
+	private IntegerProperty postalCode;
+	private StringProperty city;
+	private ObjectProperty<LocalDate> birthday;
+
+	public Person() {}
+
+	public Person(String fname, String lname)
+	{
+		this.firstName = new SimpleStringProperty(fname);
+		this.lastName = new SimpleStringProperty(lname);;
+		this.street = new SimpleStringProperty("some street");
+		this.postalCode = new SimpleIntegerProperty(123321);
+		this.city = new SimpleStringProperty("some city");
+		this.birthday = new SimpleObjectProperty<LocalDate>(LocalDate.of(1999, 2, 21));
+	}
+
+	public StringProperty firstNameProperty()
+	{
+		return firstName;
+	}
+
+	public void setFirstName(String fn)
+	{
+		this.firstName.set(fn);
+	}
+
+	public String getFirstName()
+	{
+		return this.firstName.get();
+	}
+
+	//--------------------------------------------------------------------------------------
+	public StringProperty lastNameProperty()
+	{
+		return lastName;
+	}
+
+	public String getLastName()
+	{
+		return lastName.get();
+	}
+
+	public void setLastName(String ln)
+	{
+		this.lastName.set(ln);
+	}
+
+	//--------------------------------------------------------------------------------------
+	public void streetProperty(StringProperty street)
+	{
+		this.street = street;
+	}
+
+	public String getStreet()
+	{
+		return street.get();
+	}
+
+	public void setStreet(String street)
+	{
+		this.street.set(street);;
+	}
+
+	//--------------------------------------------------------------------------------------
+	public void postalCodeProperty(IntegerProperty postalCode)
+	{
+		this.postalCode = postalCode;
+	}
+
+	public Integer getPostalCode()
+	{
+		return postalCode.get();
+	}
+
+	public void setPostalCode(Integer pc)
+	{
+		this.postalCode.set(pc);
+	}
+
+	//--------------------------------------------------------------------------------------
+	public void cityProperty(StringProperty city)
+	{
+		this.city = city;
+	}
+
+	public String getCity()
+	{
+		return city.get();
+	}
+
+	public void setCity(String city)
+	{
+		this.city.set(city);
+	}
+
+	//--------------------------------------------------------------------------------------
+	public void birthdayProperty(ObjectProperty<LocalDate> birthday)
+	{
+		this.birthday = birthday;
+	}
+
+	public LocalDate getBirthday()
+	{
+		return birthday.get();
+	}
+
+	public void setBirthday(LocalDate bd)
+	{
+		this.birthday.set(bd);
+	}
+}
---
+++ src/main/java/org/app/view/PersonOverviewController.java
@@ -0,0 +1,110 @@
+package org.app.view;
+
+import java.time.format.DateTimeFormatter;
+import java.util.Optional;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+import org.app.MainApp;
+import org.app.model.Person;
+
+public class PersonOverviewController
+{
+	@FXML private TableView<Person> personTable;
+	@FXML private TableColumn<Person, String> firstNameColumn;
+	@FXML private TableColumn<Person, String> lastNameColumn;
+	@FXML private Label firstNameLabel;
+	@FXML private Label lastNameLabel;
+	@FXML private Label streetLabel;
+	@FXML private Label cityLabel;
+	@FXML private Label postalCodeLabel;
+	@FXML private Label birthdayLabel;
+
+	/* A hook to main */
+	private MainApp mainApp;
+
+	/* The constructor is called before the initialize() method. */
+	public PersonOverviewController() {}
+
+	/**
+	 * Is called by the main application to give a reference back to itself.
+	 * @param mainApp
+	 */
+	public void setMainApp(MainApp mainApp)
+	{
+		this.mainApp = mainApp;
+
+		// populate table with data
+		personTable.setItems(mainApp.getData());
+	}
+
+	/**
+	 * Initializes the controller class. This method is automatically called
+     * after the FXML file has been loaded.
+	 */
+	@FXML
+	private void initialize()
+	{
+		// initialize person's table
+		firstNameColumn.setCellValueFactory(cell -> cell.getValue().firstNameProperty());
+		lastNameColumn.setCellValueFactory(cell -> cell.getValue().lastNameProperty());
+
+		// reset person details
+		showPersonDetails(null);
+
+		// add listener to selected item properties, executes lambda expression whenever the property is selected
+		personTable.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> showPersonDetails(newValue));
+	}
+
+	/**
+	 * Handles delete operation.
+	 * Returns deleted {@link Person} object.
+	 */
+	@FXML
+	private Optional<Person> handleDeletePerson()
+	{
+		int index = personTable.getSelectionModel().getSelectedIndex();
+		if (index >= 0)
+			return Optional.ofNullable(personTable.getItems().remove(index));
+
+		Alert alert = new Alert(AlertType.WARNING);
+		alert.initOwner(mainApp.getPrimaryStage());
+		alert.setTitle("Delete error");
+		alert.setHeaderText("No Person selected");
+		alert.setContentText("Please select a person to delete.");
+		alert.showAndWait();
+		return Optional.empty();
+	}
+
+	/**
+	 * Populates person details fields {@link Person} info.
+	 * Clears person details fields if the person is null.
+	 * @param person
+	 */
+	private void showPersonDetails(Person person)
+	{
+		if (person != null)
+		{
+			firstNameLabel.setText(person.getFirstName());
+			lastNameLabel.setText(person.getLastName());
+			streetLabel.setText(person.getStreet());
+			cityLabel.setText(person.getCity());
+			postalCodeLabel.setText(Integer.toString(person.getPostalCode()));
+			birthdayLabel.setText(person.getBirthday().format(DateTimeFormatter.ofPattern("dd-MM-yyyy")));
+		}
+		else
+		{
+			firstNameLabel.setText("");
+			lastNameLabel.setText("");
+			streetLabel.setText("");
+			cityLabel.setText("");
+			postalCodeLabel.setText("");
+			birthdayLabel.setText("");
+		}
+	}
+}
